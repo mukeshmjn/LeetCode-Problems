1052e57ffef02099a2380751d35bb2940b1b696e
Mukesh Mahajan <68895120+mukeshmjn@users.noreply.github.com>
2022-01-17 17:42:35 +0530

    Time: 136 ms (5.18%), Space: 11.5 MB (31.64%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,32 +1,62 @@
+// class Solution {
+// public:
+    
+//     void backtrack(int p, vector<string> &ans, string s, string &S){
+//         if(S.size()==s.size()){ // if our temporary array size is equal to input size, we have our answer
+//             ans.push_back(s);
+//             return;
+//         }
+//         for(int i=p;i<S.size();i++){
+//             if(isalpha(S[i])){ // if the element is alphabet, we will have to push it as uppercase as well as lowercase and then pop also as we have done in every problem till now
+//                 s.push_back(tolower(S[i]));
+//                 backtrack(i+1, ans, s, S);
+//                 s.pop_back();
+//                 s.push_back(toupper(S[i]));
+//                 backtrack(i+1, ans, s, S);
+//                 s.pop_back();
+//             }
+//             else{ // if it's a digit, we just have to push it to our temporary array and backtrack and pop off
+//                 s.push_back(S[i]);
+//                 backtrack(i+1, ans, s, S);
+//                 s.pop_back();
+//             }
+//         }
+//     }
+    
+//     vector<string> letterCasePermutation(string S) {
+//         string s="";
+//         vector<string> ans;
+//         backtrack(0, ans, s, S);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    
-    void backtrack(int p, vector<string> &ans, string s, string &S){
-        if(S.size()==s.size()){ // if our temporary array size is equal to input size, we have our answer
-            ans.push_back(s);
+    void find(string s, string S,vector<string>& res,int st){
+        if(s.size() == S.size()){
+            res.push_back(s);
             return;
         }
-        for(int i=p;i<S.size();i++){
-            if(isalpha(S[i])){ // if the element is alphabet, we will have to push it as uppercase as well as lowercase and then pop also as we have done in every problem till now
+        for(int i=st;i<S.size();i++){
+            if(isalpha(S[i])){
                 s.push_back(tolower(S[i]));
-                backtrack(i+1, ans, s, S);
+                find(s,S,res,i+1);
                 s.pop_back();
                 s.push_back(toupper(S[i]));
-                backtrack(i+1, ans, s, S);
+                find(s,S,res,i+1);
                 s.pop_back();
-            }
-            else{ // if it's a digit, we just have to push it to our temporary array and backtrack and pop off
+            } else{
                 s.push_back(S[i]);
-                backtrack(i+1, ans, s, S);
+                find(s,S,res,i+1);
                 s.pop_back();
             }
         }
     }
-    
     vector<string> letterCasePermutation(string S) {
         string s="";
-        vector<string> ans;
-        backtrack(0, ans, s, S);
-        return ans;
+        vector<string> res;
+        find(s,S,res,0);
+        return res;
     }
 };
