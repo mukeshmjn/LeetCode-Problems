9618059e877dd034b8ac034febcd3908ca114a67
Mukesh Mahajan <68895120+mukeshmjn@users.noreply.github.com>
2022-02-12 17:02:49 +0530

    Time: 54 ms (27.17%), Space: 18.7 MB (24.87%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -3,7 +3,11 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp;
-        
+             if(amount == 0) {
+            return 1;
+        }
+        if(n==0)
+          return 0;
         dp.resize(n+1,vector<int>(amount+1,-1));
         for(int i=1;i<=n;i++) dp[i][0] = 1;
         for(int i=1;i<=amount;i++) dp[0][i] = 0;
